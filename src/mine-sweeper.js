3f8e34468246bcbe655e8272a069fc5a1681095c
Sergey.Mikhalkin <sergey.mikhalkin.work@gmail.com>
2022-05-02 03:05:38 +0300

    feat: add function mine-sweeper

--- src/mine-sweeper.js
+++ src/mine-sweeper.js
@@ -23,9 +23,92 @@ const { NotImplementedError } = require('../extensions/index.js');
  *  [1, 1, 1]
  * ]
  */
-function minesweeper(/* matrix */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function minesweeper(matrix) {
+  let result = [];
+  for(let i = 0; i < matrix.length; i++) {
+    let row = []
+    for(let j = 0; j < matrix[i].length; j++) {
+      count = 0;
+      if(j == 0 && i == 0) {
+        if(matrix[i][j + 1]) count++;
+        if(matrix[i + 1][j + 1]) count++;
+        if(matrix[i + 1][j]) count++;
+        row.push(count);
+        continue;
+      } 
+      if(j == 0 && i == matrix.length - 1) {
+        if(matrix[i - 1][j]) count++;
+        if(matrix[i - 1][j + 1]) count++;
+        if(matrix[i][j + 1]) count++;
+        row.push(count);
+        continue;
+      } 
+      if(j == matrix[i].length - 1 && i == 0) {
+        if(matrix[i][j - 1]) count++;
+        if(matrix[i + 1][j - 1]) count++;
+        if(matrix[i + 1][j]) count++;
+        row.push(count);
+        continue;
+      } 
+      if(j == matrix[i].length - 1 && i == matrix.length - 1) {
+        if(matrix[i - 1][j - 1]) count++;
+        if(matrix[i][j - 1]) count++;
+        if(matrix[i - 1][j]) count++;
+        row.push(count);
+        continue;
+      } 
+      if(j == 0 && i != 0 && i != matrix.length - 1) {
+        if(matrix[i][j + 1]) count++;
+        if(matrix[i + 1][j + 1]) count++;
+        if(matrix[i + 1][j]) count++;
+        if(matrix[i - 1][j + 1]) count++;
+        if(matrix[i - 1][j]) count++;
+        row.push(count);
+        continue;
+      }
+      if(i == 0 && j != 0 && j != matrix[i].length - 1) {
+        if(matrix[i][j + 1]) count++;
+        if(matrix[i + 1][j + 1]) count++;
+        if(matrix[i + 1][j]) count++;
+        if(matrix[i + 1][j - 1]) count++;
+        if(matrix[i][j - 1]) count++;
+        row.push(count);
+        continue;
+      }
+      if(j == matrix[i].length - 1 && i != 0 && i != matrix.length - 1) {
+        if(matrix[i][j - 1]) count++;
+        if(matrix[i + 1][j - 1]) count++;
+        if(matrix[i + 1][j]) count++;
+        if(matrix[i - 1][j]) count++;
+        if(matrix[i - 1][j - 1]) count++;
+        row.push(count);
+        continue;
+      }
+      if(i == matrix.length - 1 && j != 0 && j != matrix[i].length - 1) {
+        if(matrix[i - 1][j - 1]) count++;
+        if(matrix[i][j - 1]) count++;
+        if(matrix[i - 1][j]) count++;
+        if(matrix[i - 1][j + 1]) count++;
+        if(matrix[i][j + 1]) count++;
+        row.push(count);
+        continue;
+      }
+
+      if(matrix[i][j + 1]) count++;
+      if(matrix[i - 1][j + 1]) count++;
+      if(matrix[i - 1][j]) count++;
+      if(matrix[i - 1][j - 1]) count++;
+      if(matrix[i][j - 1]) count++;
+      if(matrix[i + 1][j - 1]) count++;
+      if(matrix[i + 1][j]) count++;
+      if(matrix[i + 1][j + 1]) count++;
+
+      row.push(count);
+    }
+    result.push(row);
+  }
+
+  return result;
 }
 
 module.exports = {
