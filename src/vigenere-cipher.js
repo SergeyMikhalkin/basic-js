b9f5cdea7e19b8e93c8d333ef8017cde7a28bfad
Sergey.Mikhalkin <sergey.mikhalkin.work@gmail.com>
2022-05-02 19:38:04 +0300

    feat: add function vigenere-cipher

--- src/vigenere-cipher.js
+++ src/vigenere-cipher.js
@@ -20,13 +20,58 @@ const { NotImplementedError } = require('../extensions/index.js');
  * 
  */
 class VigenereCipheringMachine {
-  encrypt() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+
+  constructor(direct = true) {
+    this.direct = direct
   }
-  decrypt() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+
+  encrypt(text, key) {
+    if(text == undefined || key == undefined) throw Error('Incorrect arguments!');
+    let ciphertext = '';
+    key = this.addRepeats(key, text.replace(' ', '').length - key.length);
+    let keyIndex = 0;
+    for(let i = 0; i < text.length; i++) {
+      if('abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ'.includes(text[i]) == false) {
+        ciphertext += text[i];
+        continue;
+      }
+      let M = text[i].toLowerCase().charCodeAt(0) - 97;
+      let K = key[keyIndex].toLowerCase().charCodeAt(0) - 97;
+      let C = (M + K) % 26;
+      ciphertext += String.fromCharCode(97 + C);
+      keyIndex++;
+    }
+    return this.direct ? ciphertext.toUpperCase() : ciphertext.toUpperCase().split('').reverse().join('');
+  }
+  decrypt(ciphertext, key) {
+    if(ciphertext == undefined || key == undefined) throw Error('Incorrect arguments!');
+    let text = '';
+    key = this.addRepeats(key, ciphertext.replace(' ', '').length - key.length);
+    let keyIndex = 0;
+    for(let i = 0; i < ciphertext.length; i++) {
+      if('abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ'.includes(ciphertext[i]) == false) {
+        text += ciphertext[i];
+        continue;
+      }
+      let C = ciphertext[i].toLowerCase().charCodeAt(0);
+      let K = key[keyIndex].toLowerCase().charCodeAt(0);
+      let M = (C - K + 26) % 26;
+      text += String.fromCharCode(97 + M);
+      keyIndex++;
+    }
+
+    return this.direct ? text.toUpperCase() : text.toUpperCase().split('').reverse().join('');
+  }
+
+  addRepeats(key, diff) {
+    let fullKey = '';
+    let k = 0;
+    for (let i = 0; i < key.length + diff; i++) {
+      fullKey += key[k];
+      if(k == key.length - 1) k = 0;
+      else k++;
+    }
+    return fullKey;
   }
 }
 
